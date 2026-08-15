79923278e0f22ad832f2b469b317314f436b846f
Srini <KRS@Pravis-MacBook-Air.local>
2026-08-15 13:36:28 -0700

    Add analytics tracking snippets

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,28 @@ export async function generateMetadata(): Promise<Metadata> {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`${sans.variable} ${serif.variable}`}>{children}</body>
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "y2xozv2pdp");
+            `,
+          }}
+        />
+      </head>
+      <body className={`${sans.variable} ${serif.variable}`}>
+        {children}
+        <script
+          type="module"
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "2db9814c467840bfab94302ec33f34b6"}'
+        />
+      </body>
     </html>
   );
 }
